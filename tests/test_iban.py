5faee1b981dd6dba7412a62b98814faa443efdf2
Moritz Pein <moritz.pein@gmail.com>
2016-02-17 22:47:27 +0100

    assert is a statement, not a function
    same goes for python 3: https://docs.python.org/3/reference/simple_stmts.html#assert
    using it as a function can lead to unexpected results: http://stackoverflow.com/a/30770447

--- tests/test_iban.py
+++ tests/test_iban.py
@@ -13,7 +13,7 @@ def test_get_iban():
                               bank_code=bank_code,
                               account_number=account_number)
 
-    assert(expected_iban == generated_iban)
+    assert expected_iban == generated_iban
 
 
 def test_get_bank_code():
@@ -22,11 +22,11 @@ def test_get_bank_code():
 
     expected_bank_code = 37040044
 
-    assert(retrieved_bank_code == expected_bank_code)
+    assert retrieved_bank_code == expected_bank_code
 
 
 def test_get_account_number():
     iban = "DE89370400440532013000"
     expected_account_number = 532013000
     retrieved_account_number = get_account_number(iban)
-    assert(expected_account_number == retrieved_account_number)
+    assert expected_account_number == retrieved_account_number
